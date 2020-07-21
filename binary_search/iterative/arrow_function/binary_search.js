ee018d8d80b4760c6b81a72a175623fabf6fbca4
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-07-21 18:16:25 -0500

    Update binary_search.js

--- binary_search/iterative/arrow_function/binary_search.js
+++ binary_search/iterative/arrow_function/binary_search.js
@@ -1,26 +1,20 @@
 // iterative
-binary_search = (tab,c) =>{
+check_number = (tab,s) =>{
  var deb = 0;
  var fin = tab.length - 1;
- var mid = 0;
- //if( deb > fin) return -1;
- while (deb <= fin){
-    alert(deb);
-    if( deb >= fin){
-   alert("not found");
-    return -1;
-    }
-    mid = Math.floor((deb + fin)/2);
-  if(tab[mid === c]){
-    return mid;
-  }
-  if(tab[mid] < c){
-   deb = mid +1;
-  }
-  else{
-   fin = mid - 1;
-  }
- }
+ //var mid = Math.floor((deb + fin)/2);
+  while(deb <= fin){
+   var mid = Math.floor((deb + fin)/2);
+   alert("valeurs deb : " + " "+deb + "fin : " + " "+fin);
+   if((deb >= fin) && (tab[mid]!==s)) return "not found" + " " + -1;
+   if(tab[mid] === s) return "find at index :" + " "+ mid;
+   if(tab[mid] < s){
+    deb = mid + 1;
+   }
+   else{
+    fin = mid - 1;
+   }
+  } 
 }
 var tab = [1,2,3,4,5,6,7,8,9];
-binary_search(tab,7);
+check_number(tab,7);
