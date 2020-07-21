a522a08abd201638651ce8ee4a60d05864c03d1d
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-07-21 17:35:57 -0500

    Update binary_search.js

--- binary_search/iterative/function/binary_search.js
+++ binary_search/iterative/function/binary_search.js
@@ -6,12 +6,13 @@ function binary_search(tab,c){
  //if( deb > fin) return -1;
  while (deb <= fin){
     alert(deb);
-    if( deb >= fin){
+    if((deb >= fin) && (tab[mid]!==c)){
    alert("not found");
     return -1;
     }
     mid = Math.floor((deb + fin)/2);
   if(tab[mid === c]){
+   alert ("found at index :");
     return mid;
   }
   if(tab[mid] < c){
