fd888394ddc112bff575c1cb4e1ab02d96637b23
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-07-20 22:39:51 -0500

    Add files via upload

---
+++ binary_search/iterative/arrow_function/binary_search.js
@@ -0,0 +1,26 @@
+// iterative
+binary_search = (tab,c) =>{
+ var deb = 0;
+ var fin = tab.length - 1;
+ var mid = 0;
+ //if( deb > fin) return -1;
+ while (deb <= fin){
+    alert(deb);
+    if( deb >= fin){
+   alert("not found");
+    return -1;
+    }
+    mid = Math.floor((deb + fin)/2);
+  if(tab[mid === c]){
+    return mid;
+  }
+  if(tab[mid] < c){
+   deb = mid +1;
+  }
+  else{
+   fin = mid - 1;
+  }
+ }
+}
+var tab = [1,2,3,4,5,6,7,8,9];
+binary_search(tab,7);
---
+++ binary_search/recursive/arrow_function/binary_search.js
@@ -0,0 +1,38 @@
+/******************************************************/
+let recursiveFunction = (arr, x, start, end) => { 
+//var res = -1;      
+    // Base Condition 
+    if (start > end) return -1 /*false*/; 
+   
+    // Find the middle index 
+    let mid=Math.floor((start + end)/2); 
+   
+    // Compare mid with given key x 
+    if (arr[mid]===x) return  mid/*true*/; 
+          
+    // If element at mid is greater than x, 
+    // search in the left half of mid 
+    if(arr[mid] > x)  
+        return recursiveFunction(arr, x, start, mid-1); 
+    else 
+  
+        // If element at mid is smaller than x, 
+        // search in the right half of mid 
+        return recursiveFunction(arr, x, mid+1, end); 
+}  
+   
+// Driver code 
+let arr = [1, 3, 5, 7, 8, 9]; 
+let x = 9; 
+   
+if (recursiveFunction(arr, x, 0, arr.length-1) > 0) 
+    alert("Element found! index :" +" " + recursiveFunction(arr, x, 0, arr.length-1)); 
+else 
+    alert("Element not found! :" + " " + recursiveFunction(arr, x, 0, arr.length-1)); 
+/*   
+x = 9; 
+   
+if (recursiveFunction(arr, x, 0, arr.length-1)) 
+    alert("Element found!<br>"); 
+else 
+    alert("Element not found!<br>");*/
