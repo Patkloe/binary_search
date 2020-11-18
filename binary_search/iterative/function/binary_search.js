525c0a5c9bb82d2895f9f5bd79a1990e7e725e0a
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-11-18 11:46:48 -0600

    Update binary_search.js

--- binary_search/iterative/function/binary_search.js
+++ binary_search/iterative/function/binary_search.js
@@ -1,27 +1,26 @@
 // iterative
-function binary_search(tab,c){
- var deb = 0;
- var fin = tab.length - 1;
- var mid = 0;
- //if( deb > fin) return -1;
- while (deb <= fin){
-    alert(deb);
-    if((deb >= fin) && (tab[mid]!==c)){
-   alert("not found");
-    return -1;
+function search_val(tab,c){
+  var deb = 0;
+  var fin = tab.length - 1;
+  var mid = 0;
+  while(deb <= fin){
+     alert(deb +  " " + fin);
+    if((deb >= fin)&&(tab[mid]!= c)){
+       console.log("Not found");
+     return -1;
     }
     mid = Math.floor((deb + fin)/2);
-  if(tab[mid === c]){
-   alert ("found at index :");
+    if(tab[mid] == c){
+      console.log("Found");
     return mid;
-  }
-  if(tab[mid] < c){
-   deb = mid +1;
-  }
-  else{
-   fin = mid - 1;
-  }
+    }
+    if(tab[mid] > c){
+        fin = mid;
+    }
+    else{
+        deb = mid + 1;
+    }
+  } // fin while
  }
-}
-var tab = [1,2,3,4,5,6,7,8,9];
-binary_search(tab,10);
+var tab = [1,2,3,4,5,6,7];
+search_val(tab,8);
